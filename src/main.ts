a71f73050066fcf3a2e75fe570534d02cfbb0c38
alikamatu_devrel <122170675+alikamatu@users.noreply.github.com>
2025-05-26 12:07:09 +0000

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   });
     app.useGlobalFilters(new HttpExceptionFilter());
 app.enableCors({
-  origin: ["*"],
+  origin: ['http://localhost:3000', 'https://ims-backend-dusky.vercel.app'],
   credentials: false,
 });
   await app.listen(process.env.PORT ?? 1000);
