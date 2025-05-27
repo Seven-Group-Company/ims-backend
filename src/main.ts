46567549afd9d5e0adcd603ddbd6537c3eec5dd6
alikamatu_devrel <122170675+alikamatu@users.noreply.github.com>
2025-05-27 10:51:36 +0000

    cors fixes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   });
     app.useGlobalFilters(new HttpExceptionFilter());
 app.enableCors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true,
 });
   await app.listen(process.env.PORT ?? 1000);
